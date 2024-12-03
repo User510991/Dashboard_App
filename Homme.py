40183130ed02c41457445f81c167f5c0227341a4
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-03 20:32:22 +0100

    Update Homme.py

--- Homme.py
+++ Homme.py
@@ -4,10 +4,12 @@ import streamlit as st
 st.set_page_config(
     page_title="Home",  # Titre affiché dans l'onglet du navigateur
 )
-st.title("Home")
+
 def main():
     st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
     st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")
 
 if __name__ == '__main__':
     main()
+import os
+os.system("streamlit run Home.py")

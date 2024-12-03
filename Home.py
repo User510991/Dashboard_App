32ca5087f1a4d1de0221b2f8806843a074ef9172
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-03 20:01:21 +0100

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
+    st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")
+
+if __name__ == '__main__':
+    main()

3ff4c2bcffb88871f7c27e2f965de45aa7517638
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-03 20:34:00 +0100

    Update Homme.py

--- Homme.py
+++ Homme.py
@@ -1,15 +1,15 @@
-import streamlit as st
+#import streamlit as st
 
 # Définir le titre de la page
-st.set_page_config(
-    page_title="Home",  # Titre affiché dans l'onglet du navigateur
-)
+#st.set_page_config(
+#    page_title="Home",  # Titre affiché dans l'onglet du navigateur
+#)
 
-def main():
-    st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
-    st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")
+#def main():
+#    st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
+#    st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
 import os
-os.system("streamlit run Home.py")
+os.system("streamlit run blob:https://github.com/488d04c0-0336-40c1-bfe1-a39909912093")

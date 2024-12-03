99a98d0e8f4de5fa14ee795d84f54047fc2df988
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-03 20:02:08 +0100

    Update Homme.py

--- Homme.py
+++ Homme.py
@@ -1,9 +1,2 @@
-import streamlit as st
-
-def main():
-    st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
-    st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")
-
-if __name__ == '__main__':
-    main()
-      
+import os
+os.system("streamlit run Home.py")

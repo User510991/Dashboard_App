7e9f3d9b017c4fbec8970fbffe2670e7cbc21211
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-05 13:11:19 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,8 +3,7 @@ import streamlit as st
 
 st.title("Bienvenue dans l'application avec Thèmes Dynamiques")
 
-# Appliquer le thème
-chart_template = apply_theme()
+
 def main():
     st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
     st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")

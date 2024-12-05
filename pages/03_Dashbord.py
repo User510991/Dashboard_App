0d25cecdb2a21eda49f38b117d2f79254994ca65
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-05 12:35:47 +0100

    Update 03_Dashbord.py

--- pages/03_Dashbord.py
+++ pages/03_Dashbord.py
@@ -20,54 +20,6 @@ st.set_page_config(
 # Appliquer le thème global
 chart_template = apply_theme()
 #######################
-# CSS styling
-st.markdown("""
-<style>
-
-[data-testid="block-container"] {
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-top: 1rem;
-    padding-bottom: 0rem;
-    margin-bottom: -7rem;
-}
-
-[data-testid="stVerticalBlock"] {
-    padding-left: 0rem;
-    padding-right: 0rem;
-}
-
-[data-testid="stMetric"] {
-    background-color: #000000;
-    color: #FFFFFF;
-    text-align: center;
-    padding: 15px 0;
-}
-
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-</style>
-""", unsafe_allow_html=True)
 
 #################################Description du Tableau de Bord########################################
 st.title("Tableau de Bord de la Situation de L'emploi en AFrique")

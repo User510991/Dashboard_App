29345791bae2dabf099e2638c25f453fd3575a6a
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-05 12:04:44 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+# Initialisation de la session pour le thème global
+if "theme" not in st.session_state:
+    st.session_state["theme"] = "Clair"  # Thème par défaut
+
+# Page principale pour choisir le thème
+st.title("Choix du thème global")
+
+theme = st.radio(
+    "Choisissez un thème global :",
+    ("Clair", "Sombre"),
+    index=0 if st.session_state["theme"] == "Clair" else 1
+)
 def main():
     st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
     st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")

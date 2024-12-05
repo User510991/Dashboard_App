5d86b52080d2d988407baa730d6b9e0f58d2cf32
User510991 <131642525+User510991@users.noreply.github.com>
2024-12-05 12:18:16 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,17 +1,22 @@
 import streamlit as st
 
 # Initialisation de la session pour le thème global
+from theme_utils import apply_theme
+
+# Interface utilisateur pour choisir le thème global
 if "theme" not in st.session_state:
     st.session_state["theme"] = "Clair"  # Thème par défaut
 
-# Page principale pour choisir le thème
-st.title("Choix du thème global")
+st.title("Bienvenue dans l'application avec Thèmes Dynamiques")
 
-theme = st.radio(
-    "Choisissez un thème global :",
-    ("Clair", "Sombre"),
+# Choix du thème global
+st.session_state["theme"] = st.radio(
+    "Choisissez un thème global (Pages et Graphiques) :",
+    ["Clair", "Sombre"],
     index=0 if st.session_state["theme"] == "Clair" else 1
 )
+# Appliquer le thème
+chart_template = apply_theme()
 def main():
     st.header("BIENVENNUE SUR NOTRE ESPACE DE VISUALISATION DES DONNEES")
     st.title("Cette page vous donne la possibilite de naviguer entre la visualisation des donnees, les stats desc et le tableau de bord")
